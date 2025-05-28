23d84da635e963593ccc231b4c1d6cbe7c1c3741
laribenedet <larissavbenedet@gmail.com>
2025-05-28 08:57:53 -0300

    feat: add restart button/logic

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,14 @@
 import React from "react";
-import { Gamepad2 } from "lucide-react";
+import { Gamepad2, RotateCcw } from "lucide-react";
 import Board from "./components/Board.jsx";
 import { useState } from "react";
 import { BoardState } from "./types.js";
-import { checkWinner } from "./helpers/game-logic.js";
+import { checkWinner, isBoardFull } from "./helpers/game-logic.js";
 
 function App() {
   const [board, setBoard] = useState<BoardState>(Array(9).fill(null));
   const winner = checkWinner(board);
+  const isDraw = isBoardFull(board) && !winner;
 
   const currentPlayer =
     board.filter((square) => square !== null).length % 2 === 0 ? "X" : "O";
@@ -17,8 +18,13 @@ function App() {
     setBoard(board.map((square, i) => (i === index ? currentPlayer : square)));
   };
 
+  const handleRestartGame = () => {
+    setBoard(Array(9).fill(null));
+  };
+
   const getGameStatus = () => {
     if (winner) return `Player ${winner} wins!`;
+    if (isDraw) return "It's a draw!";
     return `Player ${currentPlayer}'s Turn`;
   };
 
@@ -29,14 +35,27 @@ function App() {
           <Gamepad2 className="text-pink h-8 w-8" />
           <h1 className="text-4xl font-bold text-white">Tic Tac Toe</h1>
         </div>
-
         <div>
           <p className="text-xl font-semibold text-gray-100">
             {getGameStatus()}
           </p>
         </div>
-
-        <Board board={board} handleSquareClick={handleSquareClick} />
+        <Board
+          board={board}
+          handleSquareClick={handleSquareClick}
+          winner={winner}
+        />
+        {(winner || isDraw) && (
+          <div className="mt-8 flex justify-center">
+            <button
+              onClick={handleRestartGame}
+              className="group bg-pink flex items-center gap-2 rounded-lg px-6 py-3 text-sm font-semibold text-white hover:opacity-90"
+            >
+              Restart Game
+              <RotateCcw className="transition-transform duration-500 group-hover:-rotate-180" />
+            </button>
+          </div>
+        )}
       </div>
     </main>
   );
