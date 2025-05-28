26a817554e03bdbb83600b1cfdc6b6167f309aa9
laribenedet <larissavbenedet@gmail.com>
2025-05-28 08:58:52 -0300

    style: add border color when has a winner and animations with motion

--- src/components/Board.tsx
+++ src/components/Board.tsx
@@ -1,13 +1,28 @@
 import React from "react";
-import { BoardState } from "../types.js";
+import { BoardState, Player } from "../types.js";
 import Square from "./Square.jsx";
+import { WINNING_COMBINATIONS } from "../helpers/game-logic.js";
 
 type BoardProps = {
   board: BoardState;
   handleSquareClick: (index: number) => void;
+  winner: Player | null;
 };
 
-function Board({ board, handleSquareClick }: BoardProps) {
+function Board({ board, handleSquareClick, winner }: BoardProps) {
+  const isWinner = (index: number): boolean => {
+    if (!winner) return false;
+
+    // check if the current index is a part of the winning combination
+    // and if all the squares in the combination are occupied by the winner (ex: X or O)
+    return WINNING_COMBINATIONS.some((combination) => {
+      return (
+        combination.includes(index) &&
+        combination.every((i) => board[i] === winner)
+      );
+    });
+  };
+
   return (
     <div className="mx-auto mt-5 grid max-w-[26rem] grid-cols-3 gap-4">
       {board.map((square, index) => {
@@ -16,6 +31,7 @@ function Board({ board, handleSquareClick }: BoardProps) {
             key={index}
             value={square}
             onClick={() => handleSquareClick(index)}
+            isWinner={isWinner(index)}
           />
         );
       })}
--- src/components/Square.tsx
+++ src/components/Square.tsx
@@ -1,22 +1,42 @@
 import React from "react";
 import { Player } from "../types";
+import { motion } from "motion/react";
 
 type SquareProps = {
   value: Player | null;
   onClick: () => void;
+  isWinner: boolean;
 };
 
 const getTextColor = (value: Player | null) =>
   value === "X" ? "text-pink" : "text-white";
 
-function Square({ value, onClick }: SquareProps) {
+const getBorderColor = (value: Player | null, isWinner: boolean) => {
+  if (!isWinner) return "border-gray-200";
+  if (value === "X") return "border-pink";
+  return "border-white";
+};
+
+function Square({ value, onClick, isWinner }: SquareProps) {
   return (
-    <button
-      className={`h-32 w-32 rounded-xl border-4 border-gray-200 text-4xl font-bold ${getTextColor(value)}`}
+    <motion.button
+      className={`h-32 w-32 rounded-xl border-4 border-gray-200 text-4xl font-bold ${getTextColor(value)} ${getBorderColor(value, isWinner)}`}
       onClick={onClick}
+      initial={{ scale: 0 }}
+      animate={{ scale: 1 }}
+      whileTap={{ scale: 0.95 }}
+      whileHover={{ scale: 1.05 }}
     >
-      {value}
-    </button>
+      {value && (
+        <motion.span
+          className="block"
+          initial={{ scale: 0, rotate: -180 }}
+          animate={{ scale: 1, rotate: 0 }}
+        >
+          {value}
+        </motion.span>
+      )}
+    </motion.button>
   );
 }
 
